8a2315e1cf8e815270bf782f7b217af3000380d8
Amit Netanel <netanel.amit@gmail.com>
2026-08-09 15:17:11 +0300

    update from 2026

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,4 +3,10 @@ public static class Constants
     public const string ShipTag = "Ship";
     public const string AsteroidTag = "Asteroid";
     public const string BulletTag = "Bullet";
+
+    public const string PlayerActionMap = "Player";
+    public const string RotateAction = "Rotate";
+    public const string ThrustAction = "Thrust";
+    public const string ShootAction = "Shoot";
+    public const string RestartAction = "Restart";
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,230 +1,154 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : Singleton<PlayerController>
 {
     private static readonly int Flying = Animator.StringToHash("Flying");
+
+    [Header("Ship")]
     [SerializeField] private float thrustSpeed;
     [SerializeField] private float rotationSpeed;
-    
     [SerializeField] private Bullet bulletPrefab;
     [SerializeField] private Ship ship;
 
     [Header("MobileControls")]
     [SerializeField] private RectTransform mobileControlsContainer;
-    [SerializeField] private MobileButton leftButton;
-    [SerializeField] private MobileButton rightButton;
-    [SerializeField] private MobileButton aButton;
-    [SerializeField] private MobileButton bButton;
-    
+
     private BestObjectPool<Bullet> _bulletPool;
-    
-    private bool _throttle;
-    private Vector3 _rotationDirection;
     private Vector3 _originalShipScale;
-    
-    // Mobile input state
-    private bool _mobileThrust;
-    private bool _mobileRotateLeft;
-    private bool _mobileRotateRight;
-    private bool _mobileFire;
+
+    private InputAction _rotate;
+    private InputAction _thrust;
+    private InputAction _shoot;
+    private InputAction _restart;
 
     private void Awake()
     {
+        // Project-wide actions, which the Input System enables on entering play mode.
+        var player = InputSystem.actions.FindActionMap(Constants.PlayerActionMap, throwIfNotFound: true);
+
+        _rotate = player.FindAction(Constants.RotateAction, throwIfNotFound: true);
+        _thrust = player.FindAction(Constants.ThrustAction, throwIfNotFound: true);
+        _shoot = player.FindAction(Constants.ShootAction, throwIfNotFound: true);
+        _restart = player.FindAction(Constants.RestartAction, throwIfNotFound: true);
+
         _bulletPool = new BestObjectPool<Bullet>(bulletPrefab);
-        
+
         // Cache original ship scale
         if (ship != null)
         {
             _originalShipScale = ship.transform.localScale;
         }
-        
-        SetupMobileControls();
+
+        // On-screen controls feed the same actions as the keyboard, so this only decides visibility.
+        if (mobileControlsContainer != null)
+        {
+            mobileControlsContainer.gameObject.SetActive(Application.isMobilePlatform);
+        }
     }
 
     private void Update()
     {
         // Handle restart input when game is over or won
-        if ((GameManager.Instance.GameOver || GameManager.Instance.GameWon) && Input.GetKeyDown(KeyCode.R))
+        if (GameManager.Instance.GameOver || GameManager.Instance.GameWon)
         {
-            GameManager.Instance.RestartGame();
+            if (_restart.WasPressedThisFrame())
+            {
+                GameManager.Instance.RestartGame();
+            }
+
             return;
         }
-        
-        if (!GameManager.Instance.PlayerAlive || GameManager.Instance.GameOver || GameManager.Instance.GameWon || !GameManager.Instance.GameStarted)
-            return;
-            
-        HandleThrust();
-        HandleRotation();
-        
-        HandleEffects();
 
+        if (!GameManager.Instance.PlayerAlive || !GameManager.Instance.GameStarted)
+            return;
+
+        HandleEffects();
         HandleBullets();
     }
 
     private void HandleEffects()
     {
-        if (_throttle && ! ship.EngineAudioSource.isPlaying)
+        var throttle = _thrust.IsPressed();
+
+        if (throttle && ! ship.EngineAudioSource.isPlaying)
         {
             ship.EngineAudioSource.Play();
             ship.Animator.SetBool(Flying, true);
         }
-        else if (! _throttle && ship.EngineAudioSource.isPlaying)
+        else if (! throttle && ship.EngineAudioSource.isPlaying)
         {
             ship.EngineAudioSource.Stop();
             ship.Animator.SetBool(Flying, false);
         }
     }
 
-    private void HandleRotation()
-    {
-        bool rotateRight = Input.GetKey(KeyCode.D) || _mobileRotateRight;
-        bool rotateLeft = Input.GetKey(KeyCode.A) || _mobileRotateLeft;
-        
-        if (rotateRight)
-        {
-            _rotationDirection = new Vector3(0, 0, -1);
-        }
-        else if (rotateLeft)
-        {
-            _rotationDirection = new Vector3(0, 0, 1);
-        }
-        else
-        {
-            _rotationDirection = Vector3.zero;
-        }
-    }
-
-    private void HandleThrust()
-    {
-        _throttle = Input.GetKey(KeyCode.W) || _mobileThrust;
-    }
-
     private void FixedUpdate()
     {
         if (!GameManager.Instance.PlayerAlive || GameManager.Instance.GameOver || GameManager.Instance.GameWon || !GameManager.Instance.GameStarted)
             return;
-            
-        if (_throttle)
+
+        if (_thrust.IsPressed())
         {
             var forceVector = ship.transform.up * (thrustSpeed * Time.fixedDeltaTime);
             ship.Rigidbody2D.AddForce(forceVector, ForceMode2D.Impulse);
         }
-        
-        var rotation = _rotationDirection * (rotationSpeed * Time.fixedDeltaTime);
-        ship.Rigidbody2D.MoveRotation(ship.Rigidbody2D.rotation + rotation.z);
+
+        // Negated so left input turns counter-clockwise
+        var turn = -_rotate.ReadValue<float>();
+
+        if (turn != 0f)
+        {
+            ship.Rigidbody2D.MoveRotation(ship.Rigidbody2D.rotation + turn * rotationSpeed * Time.fixedDeltaTime);
+        }
     }
-    
+
     private void HandleBullets()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || _mobileFire)
+        if (_shoot.WasPressedThisFrame())
         {
             var newBullet = _bulletPool.GetObject();
             newBullet.transform.position = ship.transform.position;
             var bulletDirection = ship.transform.up;
             newBullet.Fire(bulletDirection);
-            
-            // Reset mobile fire state (single shot)
-            _mobileFire = false;
         }
     }
-    
+
     public void ReturnBullet(Bullet bullet)
     {
         bullet.MarkAsReturned();
         bullet.Rigidbody.linearVelocity = Vector2.zero;
         bullet.Rigidbody.angularVelocity = 0f;
-        
+
         _bulletPool.ReleaseObject(bullet);
     }
-    
+
     public void DestroyShip()
     {
         ship.gameObject.SetActive(false);
-        
+
         // Stop all movement and audio
         ship.Rigidbody2D.linearVelocity = Vector2.zero;
         ship.Rigidbody2D.angularVelocity = 0f;
         ship.EngineAudioSource.Stop();
         ship.Animator.SetBool(Flying, false);
-        
-        // Reset input state
-        _throttle = false;
-        _rotationDirection = Vector3.zero;
-        ResetMobileInputState();
     }
-    
+
     public void RespawnShip()
     {
         // Reset ship position and rotation
         ship.transform.position = Vector3.zero;
         ship.transform.rotation = Quaternion.identity;
         ship.transform.localScale = _originalShipScale;
-        
+
         // Reset physics
         ship.Rigidbody2D.linearVelocity = Vector2.zero;
         ship.Rigidbody2D.angularVelocity = 0f;
-        
-        // Reset input state
-        _throttle = false;
-        _rotationDirection = Vector3.zero;
-        ResetMobileInputState();
-        
+
         // Reactivate ship
         ship.gameObject.SetActive(true);
-        
+
         // Re-register with warp manager
         ship.RegisterWithWarpManager();
-    }
-    
-    private void SetupMobileControls()
-    {
-#if UNITY_ANDROID || UNITY_IOS
-        if (mobileControlsContainer != null)
-        {
-            mobileControlsContainer.gameObject.SetActive(true);
-            SetupButtonEvents();
-        }
-#else
-        if (mobileControlsContainer != null)
-        {
-            mobileControlsContainer.gameObject.SetActive(false);
-        }
-#endif
-    }
-    
-    private void SetupButtonEvents()
-    {
-#if UNITY_ANDROID || UNITY_IOS
-        if (leftButton != null)
-        {
-            leftButton.OnPressed += () => _mobileRotateLeft = true;
-            leftButton.OnReleased += () => _mobileRotateLeft = false;
-        }
-        
-        if (rightButton != null)
-        {
-            rightButton.OnPressed += () => _mobileRotateRight = true;
-            rightButton.OnReleased += () => _mobileRotateRight = false;
-        }
-        
-        if (aButton != null)
-        {
-            aButton.OnPressed += () => _mobileThrust = true;
-            aButton.OnReleased += () => _mobileThrust = false;
-        }
-        
-        if (bButton != null)
-        {
-            bButton.OnPressed += () => _mobileFire = true;
-        }
-#endif
-    }
-    
-    private void ResetMobileInputState()
-    {
-        _mobileThrust = false;
-        _mobileRotateLeft = false;
-        _mobileRotateRight = false;
-        _mobileFire = false;
     }
 }
